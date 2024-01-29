4f214e8bc033d51181e4a9ed262bb0ed64ec4b70
Ben <ambrus.bence@gmail.com>
2024-01-29 16:00:37 +0100

    Add .config/nvim/LICENSE Add .config/nvim/config.ld Add .config/nvim/.github/CODE_OF_CONDUCT.md Add .config/nvim/.github/CONTRIBUTING.md Add .config/nvim/.github/FUNDING.yml Add .config/nvim/.github/ISSUE_TEMPLATE/bug_report.yaml Add .config/nvim/.github/ISSUE_TEMPLATE/config.yml Add .config/nvim/.github/ISSUE_TEMPLATE/feature_request.yaml Add .config/nvim/.github/PULL_REQUEST_TEMPLATE/chore.md Add .config/nvim/.github/PULL_REQUEST_TEMPLATE/feature.md Add .config/nvim/.github/PULL_REQUEST_TEMPLATE/fix.md Add .config/nvim/.github/README.md Add .config/nvim/.github/stale.yml Add .config/nvim/.github/workflows/conventional_commit.yml Add .config/nvim/.github/workflows/docs.yml Add .config/nvim/.github/workflows/luacheck.yml Add .config/nvim/.github/workflows/spell.yml Add .config/nvim/.github/workflows/style.yml Add .config/nvim/.github/workflows/updater.yml Add .config/nvim/.gitignore Add .config/nvim/.luacheckrc Add .config/nvim/.stylua.toml Add .config/nvim/.styluaignore Add .config/nvim/.typos.toml Add .config/nvim/init.lua Add .config/nvim/lua/astronvim/autocmds.lua Add .config/nvim/lua/astronvim/bootstrap.lua Add .config/nvim/lua/astronvim/health.lua Add .config/nvim/lua/astronvim/icons/nerd_font.lua Add .config/nvim/lua/astronvim/icons/text.lua Add .config/nvim/lua/astronvim/lazy.lua Add .config/nvim/lua/astronvim/mappings.lua Add .config/nvim/lua/astronvim/options.lua Add .config/nvim/lua/astronvim/utils/buffer.lua Add .config/nvim/lua/astronvim/utils/ffi.lua Add .config/nvim/lua/astronvim/utils/git.lua Add .config/nvim/lua/astronvim/utils/init.lua Add .config/nvim/lua/astronvim/utils/lsp.lua Add .config/nvim/lua/astronvim/utils/mason.lua Add .config/nvim/lua/astronvim/utils/status.lua Add .config/nvim/lua/astronvim/utils/ui.lua Add .config/nvim/lua/astronvim/utils/updater.lua Add .config/nvim/lua/lazy_snapshot.lua Add .config/nvim/lua/plugins/alpha.lua Add .config/nvim/lua/plugins/cmp.lua Add .config/nvim/lua/plugins/configs/alpha.lua Add .config/nvim/lua/plugins/configs/guess-indent.lua Add .config/nvim/lua/plugins/configs/heirline.lua Add .config/nvim/lua/plugins/configs/lspconfig.lua Add .config/nvim/lua/plugins/configs/lspkind.lua Add .config/nvim/lua/plugins/configs/luasnip.lua Add .config/nvim/lua/plugins/configs/mason-lspconfig.lua Add .config/nvim/lua/plugins/configs/mason-null-ls.lua Add .config/nvim/lua/plugins/configs/mason-nvim-dap.lua Add .config/nvim/lua/plugins/configs/mason.lua Add .config/nvim/lua/plugins/configs/notify.lua Add .config/nvim/lua/plugins/configs/nvim-autopairs.lua Add .config/nvim/lua/plugins/configs/nvim-dap-ui.lua Add .config/nvim/lua/plugins/configs/nvim-treesitter.lua Add .config/nvim/lua/plugins/configs/nvim-web-devicons.lua Add .config/nvim/lua/plugins/configs/telescope.lua Add .config/nvim/lua/plugins/configs/which-key.lua Add .config/nvim/lua/plugins/core.lua Add .config/nvim/lua/plugins/dap.lua Add .config/nvim/lua/plugins/git.lua Add .config/nvim/lua/plugins/heirline.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/mason.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/telescope.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/ui.lua Add .config/nvim/vscode_init.lua Add link-nvim-conf.bat Change attributes of .config/nvim/lua/user/init.lua Change attributes of .config/nvim/lua/user/mappings.lua Change attributes of .config/nvim/lua/user/plugins/everforest.lua Change attributes of .config/nvim/lua/user/plugins/leap.lua Change attributes of .config/nvim/lua/user/plugins/tokyo.lua

---
+++ dot_config/nvim/vscode_init.lua
@@ -0,0 +1,72 @@
+-- Plugins
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+-- Treesitter
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+Plug 'mfussenegger/nvim-ts-hint-textobject'
+
+-- Comments
+Plug 'terrortylor/nvim-comment'
+
+-- Leap
+Plug 'ggandor/leap.nvim'
+
+-- Vim surround
+Plug 'tpope/vim-surround'
+vim.call('plug#end')
+
+-- Plugin config
+require('nvim_comment').setup({
+	hook = function()
+		if vim.api.nvim_buf_get_option(0, "filetype") == "autohotkey" then
+			vim.api.nvim_buf_set_option(0, "commentstring", "; %s")
+		end
+	end
+})
+require('leap').add_default_mappings()
+
+-- Vim-surround
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+vim.cmd('let g:surround_no_mappings = 1')
+map('n', 'ds', '<Plug>Dsurround', opts)
+map('n', 'cs', '<Plug>Csurround', opts)
+map('n', 'cS', '<Plug>CSurround', opts)
+map('n', 'ys', '<Plug>Ysurround', opts)
+map('n', 'yS', '<Plug>YSurround', opts)
+map('n', 'yss', '<Plug>Yssurround', opts)
+map('n', 'ySs', '<Plug>YSsurround', opts)
+map('n', 'ySS', '<Plug>YSSurround', opts)
+map('x', 'gs', '<Plug>VSurround', opts)
+map('x', 'gS', '<Plug>VgSurround', opts)
+
+-- Keymaps
+map('n', 'K', '10k', opts)
+
+-- Settings
+local set = vim.opt
+set.number = true
+set.relativenumber = true
+
+set.autoindent = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.smarttab = true
+set.softtabstop = 4
+
+set.mouse = 'a'
+set.clipboard = 'unnamed'
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.scrolloff = 5
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+set.cursorline = true
+set.hidden = true
