1c16b895669bf48f211d035fa0d607c021f0d82e
Ben <ambrus.bence@gmail.com>
2024-01-30 09:23:07 +0100

    Update .config/nvim/lua/user/init.lua Add .config/nvim/lua/user/plugins/colorscheme.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -1,4 +1,4 @@
-Usr_colorscheme = "tokyonight"
+Usr_colorscheme = "catppuccin"
 local colorscheme_avail, _ = pcall(require, Usr_colorscheme)
 if colorscheme_avail then
   colorscheme = Usr_colorscheme
