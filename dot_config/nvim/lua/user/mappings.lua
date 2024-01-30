edc2b84a67f6ba0920f840bab60c5f51e4f6e218
Ben <ambrus.bence@gmail.com>
2024-01-30 18:10:59 +0100

    Update .config/nvim/lua/user/mappings.lua

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -2,7 +2,8 @@ local mappings = {
   n = {
     ["<Tab>"] = { "<cmd>bnext<cr>", desc = "Switch to next tab" },
     ["<S-Tab>"] = { "<cmd>bprevious<cr>", desc = "Switch to previous tab" },
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", name = "Buffers", desc = "Create new tab" },
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "Create new tab" },
+    ["<leader>bx"] = { "<cmd>tabclose<cr>", desc = "Close current tab" },
   }
 }
 
