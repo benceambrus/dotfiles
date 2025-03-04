6868560cb83e5546d48f3fca53b58861f972e102
Ben <ambrus.bence@gmail.com>
2025-03-04 10:28:35 +0100

    Update AppData/Local/nvim/init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,2 +1,18 @@
+-- Global options
+vim.opt.clipboard = "unnamedplus"
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 4
+
+vim.g.mapleader = ","
+
+-- Lazy.nvim
 require("config.lazy")
 
