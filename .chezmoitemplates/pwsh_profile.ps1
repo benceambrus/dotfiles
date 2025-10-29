bb3def83df622f8e6be6302b650e364f52d98f9c
Ben <ambrus.bence@gmail.com>
2025-10-29 19:00:50 +0100

    Automatic commit from chezmoi-git.

--- .chezmoitemplates/pwsh_profile.ps1
+++ .chezmoitemplates/pwsh_profile.ps1
@@ -1,11 +1,19 @@
 # Alias commands
 # SSH
 {{ if (and (eq .chezmoi.os "windows") (eq .chezmoi.hostname "home-desktop" )) }}
-Function SshBme { & ssh -Y BME-Desktop }
-Function SshRaspberry { & ssh raspberry }
-Function RpiGetVids { & ssh raspberry "cd /mnt/WD6TB/twc/download; find . -type f -exec basename {} \;" }
+Function SshBme
+{ & ssh -Y BME-Desktop 
+}
+Function SshRaspberry
+{ & ssh raspberry 
+}
+Function RpiGetVids
+{ & ssh raspberry "cd /mnt/WD6TB/twc/download; find . -type f -exec basename {} \;" 
+}
 
-Function TitusUtil { Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression }
+Function TitusUtil
+{ Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression 
+}
 
 Set-Alias -Name rpi -Value SshRaspberry
 Set-Alias -Name bme -Value SshBme
@@ -17,25 +25,28 @@ $env:PATH = "C:\Users\Ambri\AppData\Local\Microsoft\WinGet\Links;$env:PATH"
 {{ end }}
 
 # Chezmoi Git Alias
-Function ChezmoiGit {
-    $currDir = $pwd
-    cd ~\.local\share\chezmoi; git add .
-    git commit -m "Automatic commit from chezmoi-git."
-    git push origin main
-    cd $currDir
+Function ChezmoiGit
+{
+  $currDir = $pwd
+  cd ~\.local\share\chezmoi; git add .
+  git commit -m "Automatic commit from chezmoi-git."
+  git push origin main
+  cd $currDir
 }
 Set-Alias -Name chezmoi-git -Value ChezmoiGit
 
 # Starship
-#if (Get-Command starship) {
-#    $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
-#    Invoke-Expression (&starship init powershell)
-#}
+if (Get-Command starship)
+{
+  $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+  Invoke-Expression (&starship init powershell)
+}
 
 # Oh-my-posh
-if (Get-Command -ErrorAction SilentlyContinue oh-my-posh) {
-    oh-my-posh --config $env:USERPROFILE\.config\oh-my-posh.omp.json init pwsh | Invoke-Expression
-}
+# if (Get-Command -ErrorAction SilentlyContinue oh-my-posh)
+# {
+#   oh-my-posh --config $env:USERPROFILE\.config\oh-my-posh.omp.json init pwsh | Invoke-Expression
+# }
 
 # Editor
 $env:Editor = "nvim"
@@ -43,20 +54,21 @@ $env:Editor = "nvim"
 # Vi-mode with cursor indicator
 Set-PsReadLineOption -EditMode Vi
 $OnViModeChange = [scriptblock] {
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block.
-        Write-Host -NoNewLine "`e[1 q"
-    }
-    else {
-        # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "`e[5 q"
-    }
+  if ($args[0] -eq 'Command')
+  {
+    # Set the cursor to a blinking block.
+    Write-Host -NoNewLine "`e[1 q"
+  } else
+  {
+    # Set the cursor to a blinking line.
+    Write-Host -NoNewLine "`e[5 q"
+  }
 }
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
 
 # LFCD
-Function lfcd { lf -print-last-dir $args | Set-Location }
-Set-Alias -Name l -Value lfcd
+# Function lfcd { lf -print-last-dir $args | Set-Location }
+# Set-Alias -Name l -Value lfcd
 
 # PSFzf options
 ## Argument search and History
@@ -71,33 +83,41 @@ Set-PsFzfOption -EnableAliasFuzzySetLocation # fd
 #Set-PsFzfOption -AltCCommand $commandOverride
 
 # Yazi cd
-function y {
-    $tmp = [System.IO.Path]::GetTempFileName()
-    yazi $args --cwd-file="$tmp"
-    $cwd = Get-Content -Path $tmp -Encoding UTF8
-    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
-        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
-    }
-    Remove-Item -Path $tmp
+function y
+{
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp -Encoding UTF8
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path)
+  {
+    Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+  }
+  Remove-Item -Path $tmp
 }
 
 # Zoxide
-if (Get-Command -ErrorAction SilentlyContinue zoxide) { 
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+if (Get-Command -ErrorAction SilentlyContinue zoxide)
+{ 
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
 
 # eza
-function eza-grid { & eza --group-directories-first --icons=always }
-function eza-long { & eza --long --group-directories-first --icons=always }
-if (Get-Command -ErrorAction SilentlyContinue) {
-    Set-Alias -Name ls -Value eza-grid
-    Set-Alias -Name ll -Value eza-long
+function eza-grid
+{ & eza --group-directories-first --icons=always 
+}
+function eza-long
+{ & eza --long --group-directories-first --icons=always 
+}
+if (Get-Command -ErrorAction SilentlyContinue)
+{
+  Set-Alias -Name ls -Value eza-grid
+  Set-Alias -Name ll -Value eza-long
 }
 
 # Mamba
-if (Get-Command -ErrorAction SilentlyContinue mamba) {
-    # !! Contents within this block are managed by 'mamba shell init' !!
-    $Env:MAMBA_ROOT_PREFIX = "~\miniforge3"
-    $Env:MAMBA_EXE = "~\miniforge3\Library\bin\mamba.exe"
-    (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
-}
+# if (Get-Command -ErrorAction SilentlyContinue mamba) {
+#     # !! Contents within this block are managed by 'mamba shell init' !!
+#     $Env:MAMBA_ROOT_PREFIX = "~\miniforge3"
+#     $Env:MAMBA_EXE = "~\miniforge3\Library\bin\mamba.exe"
+#     (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+# }
