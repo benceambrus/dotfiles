4697e010ed0315828d0b18cfa7194f21615b322a
Ben <ambrus.bence@gmail.com>
2025-11-07 22:21:41 +0100

    Automatic commit from chezmoi-git.

---
+++ .chezmoiscripts/linux/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
--- .chezmoitemplates/pwsh_profile.ps1
+++ .chezmoitemplates/pwsh_profile.ps1
@@ -103,5 +103,6 @@ if (Get-Command mamba -ErrorAction SilentlyContinue)
   # !! Contents within this block are managed by 'mamba shell init' !!
   $Env:MAMBA_ROOT_PREFIX = "~\miniforge3"
   $Env:MAMBA_EXE = "~\miniforge3\Library\bin\mamba.exe"
+  & $Env:MAMBA_EXE 'config' 'set' 'changeps1' 'False'
   (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 }
