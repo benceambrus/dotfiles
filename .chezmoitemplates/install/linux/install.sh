f911f636e21cf94089e2440be969263f4e790870
Ben <ambrus.bence@gmail.com>
2025-11-08 17:09:59 +0100

    Automatic commit from chezmoi-git.

--- .chezmoitemplates/install/linux/install.sh
+++ .chezmoitemplates/install/linux/install.sh
@@ -14,33 +14,40 @@ wget -q -o ${nvim_bin} https://github.com/neovim/neovim/releases/download/v0.11.
 chmod +x ${nvim_bin}
 
 # Lazyvim dependencies
-# tree-sitter-cli
+## tree-sitter-cli
 tcli_name=tree-sitter-linux-${cpu_arch}
 wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.25.10/${tcli_name}.gz
 gunzip ${tcli_name}.gz
 chmod +x ${tcli_name}
 mv ./${tcli_name} ~/.local/bin/tree-sitter-cli
 
-# fzf
+## fzf
 fzf_name="fzf_tarball"
 wget -O ${fzf_name} https://github.com/junegunn/fzf/releases/download/v0.66.1/${fzf_name}.tar.gz
 tar -xvf ${fzf_name}
 mv fzf ~/.local/bin
 rm ${fzf_name}.tar.gz
 
-# ripgrep
+## ripgrep
 rg_name="rg_tarball"
 wget -O ${rg_name} https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-$(uname -m)-unknown-linux-gnu.tar.gz
 tar -xvf ${rg_name}
 mv ripgrep*/rg ~/.local/bin
 rm -rf ${rg_name} ripgrep*
 
-# fd
+## fd
 fd_name="fd_tarball"
 wget -O ${fd_name} https://github.com/sharkdp/fd/releases/download/v10.3.0/fd-v10.3.0-$(uname -m)-unknown-linux-gnu.tar.gz
 tar -xvf ${fd_name}
 mv fd-*/fd ~/.local/bin
 rm -rf ${fd_name} fd-*
+
+# Lazyvim
+mv ~/.config/nvim{,.bak}
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 # Oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
