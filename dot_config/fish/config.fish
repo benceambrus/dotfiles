093731149424eacc41453441ba4c8097d12d989b
Ambri <ambrus.bence@gmail.com>
2025-12-08 14:15:06 +0100

    Automatic commit from chezmoi-git.

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,13 +2,34 @@
 set -x PATH ~/.local/bin $PATH
 
 # Environment variables
-if command -q nvim; set -x EDITOR nvim; end
+if command -q nvim
+    set -x EDITOR nvim
+end
 
-# LFCD alias
-alias l=lfcd
+# Yazi cd
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 
 # Starship prompt
 # starship init fish | source
 
 # Oh-my-posh
 oh-my-posh init fish --config ~/.config/oh-my-posh.omp.yml | source
+
+# Aliases
+## Check if commands are available before setting the aliases
+function using
+    type -q $argv[1]
+end
+
+## eza
+if using eza
+    alias ls eza
+    alias ll "eza -lh"
+end
